44d6c9170eadf9c36b373556d3d7e583538f4219
Krishnakumar AP <krishnaap7@gmail.com>
2020-08-08 01:20:40 +0530

    Add files via upload

---
+++ sat_sheduler.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug  8 00:10:04 2020
+
+@author: krishna
+"""
+
+from apscheduler.schedulers.blocking import BlockingScheduler
+import requests
+import time
+
+# timestr = time.strftime("%Y%m%d-%H%M%S")
+
+def some_job():
+    timestr = time.strftime("%Y%m%d-%H%M%S")
+    print ("Saving file @")
+    print(timestr)
+    image_url = "https://mausam.imd.gov.in/Satellite/3Dasiasec_ir1.jpg"
+    r = requests.get(image_url) # create HTTP response object 
+    filename = "%s.jpg" % timestr
+    with open(filename,'wb') as f: 
+        f.write(r.content)                                                             # send a HTTP request to the server and save 
+                                                               # the HTTP response in a response object called r 
+                                                               # Saving received content as a png file in 
+                                                               # binary format 
+  
+                                                                # write the contents of the response (r.content) 
+                                                                 # to a new file in binary mode.
+     
+scheduler = BlockingScheduler()
+# scheduler.add_job(some_job, 'interval', hours=1)
+scheduler.add_job(some_job, 'interval', minutes=20)
+scheduler.start()

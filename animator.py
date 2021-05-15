98da16ef060188cabc3920796d1332cbc48f863d
Krishnakumar AP <krishnaap7@gmail.com>
2021-05-15 21:07:25 +0530

    Update and rename req.py to animator.py

--- animator.py
+++ animator.py
@@ -3,14 +3,14 @@ import numpy as np
 import glob
  
 img_array = []
-for filename in glob.glob('C:/New folder/Images/*.jpg'):
+for filename in glob.glob('C:/New folder/Images/*.jpg'):       # Path of the downloaded images         
     img = cv2.imread(filename)
     height, width, layers = img.shape
     size = (width,height)
     img_array.append(img)
  
  
-out = cv2.VideoWriter('project.avi',cv2.VideoWriter_fourcc(*'DIVX'), 15, size)
+out = cv2.VideoWriter('project.avi',cv2.VideoWriter_fourcc(*'DIVX'), 15, size) # Saving into video
  
 for i in range(len(img_array)):
     out.write(img_array[i])

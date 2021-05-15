e663cecd30a56ec2171b3ec86a7604e34e0e7769
Krishnakumar AP <krishnaap7@gmail.com>
2021-05-15 21:47:14 +0530

    Update generate.sh

--- generate.sh
+++ generate.sh
@@ -12,5 +12,5 @@ cd /home/Cast/Radar_retrieved/
 echo saving video for radar
 convert -delay 50 20200808-0*.gif Radar_retrieved.mp4
 echo saving video : Success
-cp Radar_retrieved.mp4 /home/Cast/
+
  
